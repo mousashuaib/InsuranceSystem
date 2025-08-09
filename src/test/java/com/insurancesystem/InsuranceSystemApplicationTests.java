77555894bd3f9bc449a55044797dd15ce692159c
Mousa Shuaib <mousashuaib24@gmail.com>
2025-08-09 13:58:32 +0300

    Initial commit

---
+++ src/test/java/com/insurancesystem/InsuranceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurancesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
